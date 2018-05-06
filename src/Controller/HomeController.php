4995a430263f2beb9a12d97d31efc02a2b2a7ce4
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-05-06 14:51:46 +0200

    adds SearchType form to sidebar, not working atm

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,27 +2,34 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller; 
 use App\Entity\Post;
+use App\Form\SearchType;
 
 class HomeController extends Controller
 {
     /**
     * @Route("/", name="index")
-    * @Method("GET")
+    * @Method({"GET", "POST"})
     */
-    public function index()
+    public function index(Request $request)
     {
         $entityManager = $this->getDoctrine()->getManager();
         
         $posts = $entityManager->getRepository(Post::class)->findLatest();
         $latestPosts = $entityManager->getRepository(Post::class)->findLatest();
         
+        $search_form = $this->createForm(SearchType::class);
+        
+        $search_form->handleRequest($request);
+        
         return $this->render('Home/index.html.twig', array(
             'posts' => $posts,
             'latestPosts' => $latestPosts,
+            'search_form' => $search_form->createView(),
         ));
     }
 }
