84fb1544a033a0039b4bac70d8649598e3afded2
Matthijs Braspenning <matthijsbraspenning@gmail.com>
2018-05-14 21:50:21 +0200

    sets up delete functionality for categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -14,6 +14,7 @@ use App\Entity\Post;
 use App\Utils\Search\SearchUtil;
 use App\Utils\Pagination\PaginationUtil;
 use App\Form\CategoryType;
+use App\Form\DeleteCategoryType;
 use App\Service\CategoryService;
 
 class CategoryController extends Controller
@@ -30,33 +31,7 @@ class CategoryController extends Controller
     }
     
     /**
-    * @Route("blog/category/new", name="category_create")
-    * @Security("has_role('ROLE_ADMIN')")
-    * @Method({"GET", "POST"})
-    */
-    public function create(Request $request)
-    {
-        $category = new Category();
-        
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-        
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            $category = $form->getData();
-            
-            $this->CategoryService->persist($category);
-            
-            return $this->redirectToRoute('blog_index');
-        }
-        
-        return $this->render('Category/new.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-    
-    /**
-    * @Route("blog/category/{name}/{page}", name="category_list", requirements={"page" = "\d+"})
+    * @Route("blog/category/{name}/{page}", name="category_list", requirements={"page" = "\d+", "name" = "^(?!admin)(?!new)[^/]+"})
     * @Method({"GET", "POST"})
     */
     public function index(Request $request, Category $category, int $page = 1)
@@ -86,5 +61,103 @@ class CategoryController extends Controller
             'search_form' => $search_form->createView(),
             'number_of_pages' => $number_of_pages,
         ));
+    }
+    
+    /**
+    * @Route("blog/category/new", name="category_create")
+    * @Security("has_role('ROLE_ADMIN')")
+    * @Method({"GET", "POST"})
+    */
+    public function create(Request $request)
+    {
+        $category = new Category();
+        
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $category = $form->getData();
+            
+            $this->CategoryService->persist($category);
+            
+            return $this->redirectToRoute('blog_index');
+        }
+        
+        return $this->render('Category/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+    
+    /**
+    * @Route("blog/category/admin", name="category_admin")
+    * @Security("has_role('ROLE_ADMIN')")
+    * @Method({"GET", "POST"})
+    */
+    public function admin(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $categories = $entityManager->getRepository(Category::class)->findAll();
+        
+        $delete_forms = [];
+        
+        foreach ($categories as $category)
+        {
+            $delete_forms[] = $this->createForm(DeleteCategoryType::class, $category, array(
+                'action' => $this->generateUrl('category_delete', array(
+                    'name' => $category->getName(),
+                )),
+                'method' => 'DELETE',
+            ))->createView();  
+        }
+        
+        return $this->render('Category/admin.html.twig', array(
+            'categories' => $categories,
+            'delete_forms' => $delete_forms,
+        ));
+    }
+    
+//    /**
+//    * @Route("blog/category/admin/{id}", name="category_admin")
+//    * @Security("has_role('ROLE_ADMIN')")
+//    * @Method({"GET", "POST"})
+//    */
+//    public function show(Request $request, Category $category)
+//    {
+//        $delete_form = $this->createForm(DeleteCategoryType::class, $category, array(
+//                'action' => $this->generateUrl('category_delete', array(
+//                    'id' => $category->getId(),
+//                )),
+//                'method' => 'DELETE',
+//            ));
+//        
+//        return $this->render('Category/show.html.twig', array(
+//            'category' => $category,
+//            'delete_form' => $delete_form->createView(),
+//        ));
+//    }
+    
+    /**
+    * @Route("blog/category/{name}", name="category_delete")
+    * @Security("has_role('ROLE_ADMIN')")
+    * @Method({"DELETE"})
+    */
+    public function delete(Request $request, Category $category)
+    {
+        $form = $this->createForm(DeleteCategoryType::class, $category, array(
+            'action' => $this->generateUrl('category_delete', array(
+                'name' => $category->getName(),
+            )),
+            'method' => 'DELETE',
+        ));
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->CategoryService->remove($category);
+        }
+        
+        return $this->redirectToRoute('blog_index');
     }
 }
--- src/Form/DeleteCategoryType.php
+++ src/Form/DeleteCategoryType.php
@@ -5,21 +5,21 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use App\Entity\Post;
+use App\Entity\Category;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class DeletePostType extends AbstractType
+class DeleteCategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('submit', SubmitType::class, array('label' => 'Delete Post'));
+            ->add('submit', SubmitType::class, array('label' => 'Delete Category'));
     }
     
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => Post::class,
+            'data_class' => Category::class,
         ));
     }
 }
